b589b23ab0d39492411fca95e508e81ad50819c2
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-05-14 23:45:39 +0800

    Add page object for DuckDuckGo search page and result page. No implementation yet. Only stub

---
+++ pages/result.py
@@ -0,0 +1,21 @@
+"""
+This module contains DuckDuckGo Result Page
+the page object for the DuckDuckGo search result
+"""
+
+class DuckDuckGoResultPage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def result_link_titles(self):
+        #TODO
+        return []
+
+    def search_input_value(self):
+        #TODO
+        return ""
+
+    def title(self):
+        #TODO
+        return ""
---
+++ pages/search.py
@@ -0,0 +1,18 @@
+"""
+This module contains DuckDuckGo Search page
+the page object for the DuckDuckgo search page.
+"""
+
+class DuckDuckGoSearchPage:
+    
+    #constructor method 
+    def __init__(self,browser):
+        self.browser = browser
+
+    def load(self):
+        # TODO
+        pass
+
+    def search(self, phrase):
+        #TODO
+        pass
