8ef4cca80a0d569f9cb48cf91a8df95872a0ad0f
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-05-19 23:19:37 +0800

    Add config file (in json) for multiple browser support and implement the features in the pytest fixture file

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,39 @@ This module contains shared fixtures
 
 import pytest
 import selenium.webdriver
+import json
 
 @pytest.fixture
-def browser():
-    PATH = "F:\Selenium-introduction\chromedriver.exe"
-    # Initialize the chromewebdriver instance
-    b = selenium.webdriver.Chrome(PATH)
+def config(scope="session"):
+    
+    # Read the json file
+    with open("config.json") as config_file:
+        config = json.load(config_file)
 
+    # Assert values are acceptable : Validate value
+    assert config["browser"] in ["Firefox", "Chrome", "Headless Chrome"]
+    assert isinstance(config["implicit_wait"], int)
+    assert config["implicit_wait"] > 0
+
+    # Return config so it can be used
+    return config    
+
+@pytest.fixture
+def browser(config):
+    # Initialize the chromewebdriver instance
+    if config["browser"] == "Firefox":
+        b = selenium.webdriver.Firefox()
+    elif config["browser"] == "Chrome":
+        b = selenium.webdriver.Chrome()
+    elif config["browser"] == "Headless Chrome":
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        b = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser {config["browser"]} is not supported.')
+    
     # Make its call wait up to 10s for elements to appear
-    b.implicitly_wait(10)
+    b.implicitly_wait(config["implicit_wait"])
 
     # return the webdriver for the setup 
     yield b
