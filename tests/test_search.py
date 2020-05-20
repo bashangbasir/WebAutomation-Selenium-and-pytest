808f963cb9a84091fdf8414fbb77cc57672c600c
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-05-20 22:46:30 +0800

    Add parallel test and add readme.md file

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,32 +1,33 @@
 """
 These test cover DuckDuckGo searches
 """
+import pytest
 from pages.search import DuckDuckGoSearchPage 
 from pages.result import DuckDuckGoResultPage
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize("phrase",["panda", "python","polar bear"])
+def test_basic_duckduckgo_search(browser,phrase):
     
     # initialize page object and phrase to search
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
 
     # given duckduckgo homepage
     search_page.load()
 
     # when user searches for "panda"
-    search_page.search(PHRASE)
+    search_page.search(phrase)
 
-    # and the search result query is "panda"
-    assert PHRASE == result_page.search_input_value()
+    # then the search result query is "panda"
+    assert phrase == result_page.search_input_value()
 
     # and the search result links pertain to "panda"
     titles = result_page.result_link_titles()
     matches = []
     for t in titles:
-        if PHRASE.lower() in t.lower():
+        if phrase.lower() in t.lower():
             matches.append(t)
     assert len(matches)>0 # return true when there is title in the matches
 
-    # then search result title contain "panda"
-    assert PHRASE in result_page.title()
+    # And search result title contain "panda"
+    assert phrase in result_page.title()
